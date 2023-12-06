d3a72122b5236d6a0750372916864e0524ec1c54
zafar <zafarfeb17@gmail.com>
2023-12-05 23:58:23 -0500

    added javadoc and financial history for seller

--- JavaCartPro/src/model/Discount.java
+++ JavaCartPro/src/model/Discount.java
@@ -1,5 +1,10 @@
 package JavaCartPro.src.model;
 
 public interface Discount {
+
+    /**
+     * applies a discount to a purchase
+     * @param purchase transaction to apply a discount to
+     */
     void applyDiscount(Transaction purchase);
 }
